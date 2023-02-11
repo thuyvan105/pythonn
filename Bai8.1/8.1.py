98318a7e00c23847ced416bc2171be1e0268f30c
thuyvan105 <125102374+thuyvan105@users.noreply.github.com>
2023-02-11 08:49:17 +0700

    Create 8.1.py

---
+++ Bai8.1/8.1.py
@@ -0,0 +1,9 @@
+max  =  int ( input ( " Vui lòng nhập Giá trị lớn nhất : " ))
+tổng  =  0
+
+for  number  in  range ( 1 , max + 1 ):
+    if ( number  %  2  != 0 ):
+        in ( "(0" . . format ( number ))
+        tổng  +=  số
+
+print ( "Tổng số các ngày từ 1 đến (0) = {1}" . format ( số , tổng ))
